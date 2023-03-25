21a345588416af7cf3d5f337c8418909a5147cf8
Umair-Jamaat-Ali <umairansari758@gmail.com>
2023-03-25 12:01:09 +0500

    js class practice2

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 // write a js program, that check user input number is even or odd?
 
-var x =+ prompt ("Enter a number");
+// var x =+ prompt ("Enter a number");
 
-if(x%2==0){
-    console.log("Number is even");
+// if(x%2==0){
+//     console.log("Number is even");
+// }
+// else{
+// console.log("Number is odd")
+// }
+
+
+var y =+ prompt("Entre your code");
+
+var y = 10927;
+
+if(y=10927){
+console.log("Congratulation");
 }
 else{
-console.log("Number is odd")
+    console.log("Best luck for next time");
 }
